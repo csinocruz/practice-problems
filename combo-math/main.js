54a30fbd92edb7a7067968e17af4bb9cc8d4ac63
Cesca <csinocruz@gmail.com>
2018-01-06 13:09:05 -0800

    function comboMath is working

---
+++ combo-math/main.js
@@ -0,0 +1,11 @@
+
+
+function comboMath(num1, num2, num3, num4, num5) {
+    var result = num1 * num2;
+        result += num3;
+        result /= num4;
+        result -= num5;
+        return result;
+};
+
+console.log(comboMath(3,2,6,3,5));
