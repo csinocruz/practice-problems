75b6352ccd493ee17ff2aa5caa20f0ec39aec32c
Cesca <csinocruz@gmail.com>
2018-01-06 13:25:38 -0800

    function handles if input is NaN

--- combo-math/main.js
+++ combo-math/main.js
@@ -1,11 +1,19 @@
-
+var c = 'combo';
 
 function comboMath(num1, num2, num3, num4, num5) {
-    var result = num1 * num2;
+    if(!isNaN(num1) && 
+       !isNaN(num2) && 
+       !isNaN(num3) && 
+       !isNaN(num4) && 
+       !isNaN(num5) ) {
+        var result = num1 * num2;
         result += num3;
         result /= num4;
         result -= num5;
         return result;
+    } else {
+        return 'Please input numbers only';
+    }
 };
 
 console.log(comboMath(3,2,6,3,5));
